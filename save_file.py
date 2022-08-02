b0bcc529f993e4c6d49ae94baf251f412a39b584
Tatyana <valchik.tany28@gmail.com>
2022-08-02 23:56:32 +0300

    добавили функцию сохранения и эмоджи

---
+++ save_file.py
@@ -0,0 +1,37 @@
+import json
+
+data = {}
+data['people'] = []
+data['people'].append({
+    'name': 'Scott',
+    'website': 'stackabuse.com',
+    'from': 'Nebraska'
+})
+data['people'].append({
+    'name': 'Larry',
+    'website': 'google.com',
+    'from': 'Michigan'
+})
+data['people'].append({
+    'name': 'Tim',
+    'website': 'apple.com',
+    'from': 'Alabama'
+})
+data['people'].append({
+    'name': 'Ludmila',
+    'website': 'google.com',
+    'from': 'Rassha'
+})
+# with open('data.txt', 'w') as outfile:
+#     json.dump(data, outfile)
+# print(data,"\n")
+
+
+def save():
+    with open("data_file.json", "w", encoding='utf-8') as outfile:  
+        json.dump(data, outfile)
+print(data)
+print('Данные успешно сохранены!!!',"\U0001f600")
+
+# python -m pip install pythonji - для добавления эмоджи (прописать команду вконсоли)
+
