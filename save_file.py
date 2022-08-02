4140f43ea55bdf7e4146d029f813b1d564efb130
Tatyana <valchik.tany28@gmail.com>
2022-08-03 01:39:35 +0300

    успешное сохранение данных

--- save_file.py
+++ save_file.py
@@ -18,15 +18,20 @@ data['people'].append({
     'website': 'google.com',
     'from': 'Rassha'
 })
+data['people'].append({
+    'name': 'Valera',
+    'website': 'google.com',
+    'from': 'Rassha'
+})
 # with open('data.txt', 'w') as outfile:
 #     json.dump(data, outfile)
 # print(data,"\n")
 
 
 def save():
-    with open("data_file.json", "w", encoding='utf-8') as outfile:  
-        json.dump(data, outfile)
-print(data)
+    with open("data_file.json", "w", encoding='utf-8') as file:  
+        json.dump(data, file)
 print('Данные успешно сохранены!!!',"\U0001f600")
+save()
 
 # python -m pip install pythonji - для добавления эмоджи (прописать команду вконсоли)
